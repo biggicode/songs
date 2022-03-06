1b726b765270d1843c656cc29d897f3a21ac6bc9
biggicode <larioncescu@yahoo.com>
2022-03-06 12:27:43 +0200

    songs reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "All Stars", duration: "3:15" },
+    { title: "Macarena", duration: "2:30" },
+    { title: "I Want it That Way", duration: "1:45" },
+  ]
+}
